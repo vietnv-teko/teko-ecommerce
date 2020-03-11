cdb17fd2ba1c015e72eb3e9d2186dcc97a3d0ad5
luchn <luc.hn@teko.vn>
2020-03-11 15:45:54 +0700

    ..

--- src/components/ProductItem/ProductItem.stories.tsx
+++ src/components/ProductItem/ProductItem.stories.tsx
@@ -6,4 +6,15 @@ export default {
   component: ProductItem,
 };
 
-export const Hi = () => <ProductItem />;
+const product = {
+  sku: '123',
+  name: 'Laptop ASUS ROG Strix G G531GD-AL034T',
+  price: {
+    sellPrice: 23490000,
+    supplierSalePrice: 24990000,
+  },
+  image:
+    'https://lh3.googleusercontent.com/apZl8ZCL6qhElaV_a2oM11GntPHaaOUXmXTtHFYfCgsEzBJnKnoFlv-rhF_VUhqWUgz8Q7rnNjpyoCt9syY',
+};
+
+export const Default = () => <ProductItem product={product} />;
--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,19 +1,49 @@
-import * as React from 'react';
+import React from 'react';
 import styles from './ProductItem.module.css';
+import classname from 'classnames/bind';
+let cx = classname.bind(styles);
 
-export type ProductItem = {
+interface ProductItemProps {
   /**
    * props test
    * */
-  props1?: string;
+  product: {
+    sku: string;
+    name: string;
+    price: {
+      sellPrice: number;
+      supplierSalePrice: number;
+    };
+    image: string;
+    status?: string;
+  };
+  className?: string;
   /**
    * Simple click handler
    */
   onClick?: () => void;
-};
+}
 
-const ProductItem = ({ props1 = '1' }: ProductItem) => {
-  return <div className={styles.divider}>divider {props1}</div>;
+const ProductItem: React.FC<ProductItemProps> = props => {
+  const { className, onClick, product } = props;
+  return (
+    <div onClick={() => onClick} className={cx('product-item', className)}>
+      <div className={cx('product-image')}>
+        <img src={product.image} alt="" />
+        {product.status && (
+          <div className={cx('product-status')}>{product.status}</div>
+        )}
+      </div>
+      <div className={cx('product-name')}>{product.name}</div>
+      <div className={cx('shipping-info')}>
+        <span>Miễn phí giao hàng toàn quốc</span>
+      </div>
+      <div className={cx('product-price')}>
+        <div className={cx('final-price')}>{product.price.sellPrice}</div>
+        <div className={cx('old-price')}>{product.price.supplierSalePrice}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductItem;
