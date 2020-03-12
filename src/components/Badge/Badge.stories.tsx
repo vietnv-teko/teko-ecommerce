0d36a006a26de528d641d01fa6ef267e6eeaca93
Đức Hiếu Trần <hieu.td@teko.vn>
2020-03-12 17:46:03 +0700

    Badge done

---
+++ src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Badge from './Badge';
+
+export default {
+  title: 'Badge',
+};
+
+export const SecondaryBadge = () => (
+  <Badge value={9} color={'secondary'}>
+    Inbox
+  </Badge>
+);
+
+export const PrimaryBadge = () => (
+  <Badge value={100} color={'primary'}>
+    Mail
+  </Badge>
+);
---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Badge.module.scss';
+
+let cx = classNames.bind(styles);
+
+interface IBadge {
+  color?: any;
+  value: number | string;
+}
+
+const Badge: React.FC<IBadge> = props => {
+  let { children, color = 'primary', value } = props;
+
+  if (typeof value === 'string') value = parseInt(value);
+
+  return (
+    <div className={cx('container', color)}>
+      {children}
+      <span className={cx({ badge: true, round: value < 10 })}>
+        {value < 100 ? value : '99+'}
+      </span>
+    </div>
+  );
+};
+
+export default Badge;
