8caa7bd4d441fba5c2a3d2be1d2f13c0777cef71
Đức Hiếu Trần <hieu.td@teko.vn>
2020-03-12 17:47:58 +0700

    Badge custom class

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -7,15 +7,16 @@ let cx = classNames.bind(styles);
 interface IBadge {
   color?: any;
   value: number | string;
+  className?: string;
 }
 
 const Badge: React.FC<IBadge> = props => {
-  let { children, color = 'primary', value } = props;
+  let { children, color = 'primary', value, className } = props;
 
   if (typeof value === 'string') value = parseInt(value);
 
   return (
-    <div className={cx('container', color)}>
+    <div className={cx('container', color, className)}>
       {children}
       <span className={cx({ badge: true, round: value < 10 })}>
         {value < 100 ? value : '99+'}
