8dd1080511b1d0b67fde445454d3d0d58caab343
Đức Hiếu Trần <hieu.td@teko.vn>
2020-03-12 15:32:45 +0700

    Button default props value

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -7,14 +7,17 @@ export default {
 
 export const DefaultButton = () => <Button>Default Button</Button>;
 
-export const SmallButton = () => <Button size="small">Small Button</Button>;
-export const LargeButton = () => <Button size="large">Large Button</Button>;
-
-export const PrimaryContainedButton = () => (
-  <Button size="medium" color="primary" variant="contained">
-    Primary Contained Button
+export const SmallButton = () => (
+  <Button size="small" variant="outlined">
+    Small Button
   </Button>
 );
+export const LargeButton = () => (
+  <Button size="large" variant="contained">
+    Large Button
+  </Button>
+);
+
 export const SecondaryOutlinedButton = () => (
   <Button size="medium" color="secondary" variant="outlined">
     Secondary Outlined Button
