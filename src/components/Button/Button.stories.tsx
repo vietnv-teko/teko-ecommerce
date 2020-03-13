d5f0db979ae85641d0f418c0829cb148e8442fc0
Đức Hiếu Trần <hieu.td@teko.vn>
2020-03-13 16:22:15 +0700

    custom color

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -8,18 +8,31 @@ export default {
 export const DefaultButton = () => <Button>Default Button</Button>;
 
 export const SmallButton = () => (
-  <Button size="small" variant="outlined">
+  <Button
+    size="small"
+    variant="outlined"
+    rippleColor={'rgba(22, 105, 168, 0.2)'}
+  >
     Small Button
   </Button>
 );
 export const LargeButton = () => (
-  <Button size="large" variant="contained">
+  <Button
+    size="large"
+    variant="contained"
+    rippleColor={'rgba(255, 255, 255, 0.4)'}
+  >
     Large Button
   </Button>
 );
 
 export const SecondaryOutlinedButton = () => (
-  <Button size="medium" color="secondary" variant="outlined">
+  <Button
+    size="medium"
+    variant="outlined"
+    rippleColor={'rgba(224, 3, 21, 0.2)'}
+    color={'#d60315'}
+  >
     Secondary Outlined Button
   </Button>
 );
