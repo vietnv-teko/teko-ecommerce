0d05abbf057712ddac0ca7e150902db6a4ae68cc
Luc Hoang <luc.hn@teko.vn>
2020-03-17 22:02:02 +0700

    category list

--- src/components/Mobile/HeaderBlock/HeaderBlock.tsx
+++ src/components/Mobile/HeaderBlock/HeaderBlock.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import classname from 'classnames/bind';
 import styles from './HeaderBlock.module.scss';
+import { cleanObject } from 'helpers';
 let cx = classname.bind(styles);
 
 interface IHeaderBlock {
   className?: string;
+  backgroundColor?: string;
 }
 
 const HeaderBlock: React.FC<IHeaderBlock> = props => {
-  const { className, children } = props;
+  const { className, children, backgroundColor } = props;
 
-  return <div className={cx('header-block', className)}>{children}</div>;
+  return <div style={cleanObject({
+    backgroundColor: backgroundColor
+  })} className={cx('header-block', className)} > {children}</div >;
 };
 
 export default HeaderBlock;
---
+++ src/stories/HorizontalProductList.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import HorizontalProductList from 'components/Mobile/HorizontalProductList';
+import ProductItem from 'components/Common/ProductItem';
+
+export default {
+  title: "Mobile/HorizontalProductList"
+}
+
+const indents: any = [];
+
+const product = {
+  sku: '123',
+  name: 'Laptop ASUS ROG Strix G G531GD-AL034T',
+  price: {
+    sellPrice: 23490000,
+    supplierSalePrice: 24990000,
+  },
+  images: [
+    {
+      priority: 1,
+      path: '',
+      url:
+        'https://lh3.googleusercontent.com/apZl8ZCL6qhElaV_a2oM11GntPHaaOUXmXTtHFYfCgsEzBJnKnoFlv-rhF_VUhqWUgz8Q7rnNjpyoCt9syY',
+    },
+  ],
+};
+
+for (let i = 0; i < 10; i++) {
+  indents.push(
+    <ProductItem
+      type="grid"
+      img={product.images[0].url}
+      discount="5%"
+      name={product.name}
+      finalPrice={product.price.sellPrice}
+      oldPrice={product.price.supplierSalePrice}
+      sellerShippingInfo="Miễn phí vận chuyển"
+    />,
+  );
+}
+
+export const Default = () => (
+  <div style={{ width: '375px' }}>
+    <HorizontalProductList title="Sản phẩm vừa xem" link="/">
+      {indents}
+    </HorizontalProductList>
+  </div>
+);
